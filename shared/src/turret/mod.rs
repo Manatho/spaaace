eec0366d6537cd1c4e294824403d1221328d1929
Daniel Holst <notholst@gmail.com>
2023-02-27 20:18:53 +0100

    Fixed bullet rotation

--- shared/src/turret/mod.rs
+++ shared/src/turret/mod.rs
@@ -5,9 +5,10 @@ use std::{f32::consts::PI, time::Instant};
 
 use bevy::{
     prelude::{
-        default, shape, App, Assets, Color, Commands, Component, Entity, EventReader,
-        GlobalTransform, IntoSystemDescriptor, Mesh, Parent, PbrBundle, Plugin, Quat, Query, Res,
-        ResMut, StandardMaterial, SystemSet, Transform, Vec3, With, Without,
+        default, shape, App, Assets, BuildChildren, Color, Commands, Component, Entity,
+        EventReader, GlobalTransform, IntoSystemDescriptor, Mesh, PbrBundle, Plugin, Quat,
+        Query, Res, ResMut, SpatialBundle, StandardMaterial, SystemSet, Transform, Vec3, With,
+        Without,
     },
     time::Time,
     transform::TransformBundle,
@@ -189,7 +190,7 @@ fn fire_weapons_server(
 
             let bullet_transform = TransformBundle::from_transform(transform);
             let bullet = Bullet {
-                speed: 20.,
+                speed: 100.,
                 lifetime: time.elapsed_seconds() + 2.0,
             };
             commands
@@ -235,28 +236,39 @@ fn on_bullet_spawned_client(
                 rotation,
             } => {
                 let entity_id = commands
-                    .spawn(PbrBundle {
-                        mesh: meshes.add(Mesh::from(shape::Capsule {
-                            depth: 0.5,
-                            radius: 0.1,
-                            ..Default::default()
-                        })),
-                        material: materials.add(StandardMaterial {
-                            base_color: Color::BLACK,
-                            perceptual_roughness: 1.,
-                            emissive: Color::rgb(1., 0.2, 0.2) * 5.,
+                    .spawn((
+                        SpatialBundle {
+                            transform: Transform {
+                                translation: *position,
+                                rotation: *rotation,
+                                ..default()
+                            },
                             ..default()
-                        }),
-                        transform: Transform {
-                            translation: *position,
-                            rotation: *rotation * Quat::from_rotation_x(PI / 2.0),
-                            ..Default::default()
                         },
-                        ..Default::default()
-                    })
-                    .insert(Bullet {
-                        lifetime: 0.0,
-                        speed: 0.0,
+                        Bullet {
+                            lifetime: 0.0,
+                            speed: 0.0,
+                        },
+                    ))
+                    .with_children(|parent| {
+                        parent.spawn(PbrBundle {
+                            mesh: meshes.add(Mesh::from(shape::Capsule {
+                                depth: 0.5,
+                                radius: 0.1,
+                                ..Default::default()
+                            })),
+                            material: materials.add(StandardMaterial {
+                                base_color: Color::BLACK,
+                                perceptual_roughness: 1.,
+                                emissive: Color::rgb(1., 0.2, 0.2) * 5.,
+                                ..default()
+                            }),
+                            transform: Transform {
+                                rotation: Quat::from_rotation_x(PI / 2.0),
+                                ..Default::default()
+                            },
+                            ..Default::default()
+                        });
                     })
                     .id();
 
